b7d6cd7d9c48b984afb9e004cb12686c33210a68
Techavit Srichamun <por508te@gmail.com>
2025-06-19 09:53:25 +0700

    add caed V.3

--- Sever/index.js
+++ Sever/index.js
@@ -4,7 +4,7 @@
     "name": "D-BT01-1",
     "price": 190,
     "image": "https://www.thaivanguard.net/wp-content/uploads/2024/07/MOCK_VGD_BT01-1-1024x864.png",
-    "description": "This is a detailed description for Product 1."
+    "description": "Nation/Clan : Dragon Empire, Keter Sanctuary, Stoicheia"
   },
 
   {
@@ -12,7 +12,7 @@
     "name": "D-BT-01-2",
     "price": 150,
     "image": "https://www.thaivanguard.net/wp-content/uploads/2024/07/MOCK_VGD_BT01-2-1024x868.png",
-    "description": "This is a detailed description for Product 1."
+    "description": "Nation/Clan : Brandt Gate, Dark States"
   },
 
   {
@@ -20,7 +20,7 @@
     "name": "D-BT02-1",
     "price": 190,
     "image": "https://www.kidzandkai.com/wp-content/uploads/2024/10/kidkai.Vanguard-%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B9%8C%E0%B8%94%E0%B9%81%E0%B8%A7%E0%B8%99%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B9%8C%E0%B8%94%E0%B8%94%E0%B8%B5-D-BT02-1-A-Brush-with-the-Legends.jpg",
-    "description": "This is a detailed description for Product 1."
+    "description": "Nation/Clan : Brandt Gate, Dragon Empire, Stoicheia"
   },
 
   {
@@ -28,7 +28,7 @@
     "name": "D-BT-02-2",
     "price": 150,
     "image": "https://www.kidzandkai.com/wp-content/uploads/2024/10/kidkai.Vanguard-%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B9%8C%E0%B8%94%E0%B9%81%E0%B8%A7%E0%B8%99%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B9%8C%E0%B8%94%E0%B8%94%E0%B8%B5-D-BT02-2-A-Brush-with-the-Legends.jpg",
-    "description": "This is a detailed description for Product 1."
+    "description": "Nation/Clan : Dark States, Keter Sanctuary"
   },
 
   {
@@ -36,7 +36,7 @@
     "name": "D-BT03-1",
     "price": 190,
     "image": "https://www.kidzandkai.com/wp-content/uploads/2024/12/kidkai.Vanguard-%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B9%8C%E0%B8%94%E0%B9%81%E0%B8%A7%E0%B8%99%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B9%8C%E0%B8%94%E0%B8%94%E0%B8%B5-D-BT03-1-Advance-of-Intertwined-Stars.jpg",
-    "description": "This is a detailed description for Product 1."
+    "description": "Nation/Clan : Dragon Empire, Keter Sanctuary, Stoicheia"
   },
 
   {
@@ -44,7 +44,7 @@
     "name": "D-BT-03-2",
     "price": 150.,
     "image": "https://www.kidzandkai.com/wp-content/uploads/2024/12/kidkai.Vanguard-%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B9%8C%E0%B8%94%E0%B9%81%E0%B8%A7%E0%B8%99%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B9%8C%E0%B8%94%E0%B8%94%E0%B8%B5-D-BT03-2-Advance-of-Intertwined-Stars.jpg",
-    "description": "This is a detailed description for Product 1."
+    "description": "Nation/Clan : Brandt Gate, Dark States"
   },
 
   {
@@ -52,7 +52,7 @@
     "name": "D-BT-04-1",
     "price": 190,
     "image": "https://www.thaivanguard.net/wp-content/uploads/2025/02/MOCK_VGD_BT04_1.5.png",
-    "description": "This is a detailed description for Product 1."
+    "description": "Nation/Clan : Dark States, Dragon Empire, Stoicheia"
   },
 
   {
@@ -60,7 +60,7 @@
     "name": "D-04-2",
     "price": 150,
     "image": "https://www.thaivanguard.net/wp-content/uploads/2025/02/MOCK_VGD_BT04_2.5.png",
-    "description": "This is a detailed description for Product 1."
+    "description": "Nation/Clan : Brandt Gate, Keter Sanctuary"
   },
 
   {
@@ -68,7 +68,7 @@
     "name": "D-BT-05-1",
     "price": 190,
     "image": "https://www.thaivanguard.net/wp-content/uploads/2025/06/MOCK_VGD_BT05_1-1024x842.png",
-    "description": "This is a detailed description for Product 1."
+    "description": "Nation/Clan : Brandt Gate, Dark States, Dragon Empire, Keter Sanctuary"
   },
 
   {
@@ -76,6 +76,6 @@
     "name": "D-BT05-2",
     "price": 150,
     "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQU6QRXueFO6veqppdpHjifawmYlju0mFNWA&s",
-    "description": "This is a detailed description for Product 1."
+    "description": "Nation/Clan : Dragon Empire, Keter Sanctuary, Stoicheia"
   },  
 ]
